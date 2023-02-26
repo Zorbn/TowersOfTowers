36dccbef438f9426eb25c647f7b485c8eec79595
Zorbn <public.zorb@gmail.com>
2023-02-25 23:55:47 -0800

    Allow custom ports

--- Server/src/main.ts
+++ Server/src/main.ts
@@ -3,7 +3,7 @@ import * as http from "http";
 import { resolve } from "path";
 import { Server, Socket } from "socket.io";
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 const httpServer = http.createServer(app);
